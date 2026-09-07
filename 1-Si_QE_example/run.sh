d1dce162a2aedb2d009141a8ac1fd172ced280ce
Chengyan Zhang <chengyan26.zhang@gmail.com>
2026-09-07 17:38:02 -0400

    Si example

---
+++ 1-Si_QE_example/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=qe-scf
+#SBATCH --partition=day
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:30:00
+#SBATCH --mem-per-cpu=5G
+#SBATCH --output=scf.out
+
+module reset
+module load QuantumESPRESSO
+
+srun --ntasks=4 --cpus-per-task=1 pw.x -in scf.in
