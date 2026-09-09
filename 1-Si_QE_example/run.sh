450a870d1d6d1d3d4129abc46d85c222ad63255a
Chengyan Zhang <chengyan26.zhang@gmail.com>
2026-09-08 20:21:15 -0400

    modify the inputs

--- 1-Si_QE_example/run.sh
+++ 1-Si_QE_example/run.sh
@@ -5,11 +5,11 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=4
 #SBATCH --cpus-per-task=1
-#SBATCH --time=00:30:00
+#SBATCH --time=00:05:00
 #SBATCH --mem-per-cpu=5G
-#SBATCH --output=scf.out
+#SBATCH --output=Si.scf.out
 
 module reset
 module load QuantumESPRESSO
 
-srun --ntasks=4 --cpus-per-task=1 pw.x -in scf.in
+srun --ntasks=4 --cpus-per-task=1 pw.x -in Si.scf.in
